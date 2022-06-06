71bdc806710d04cfd032b8c765b078c2d56a1066
Bierkenig <92359399+Bierkenig@users.noreply.github.com>
2022-06-06 16:51:24 +0200

    added getRandomQuote method

--- src/index.php
+++ src/index.php
@@ -1,9 +1,37 @@
 <?php
 
+use Doctrine\DBAL\DriverManager;
+
 require_once '../vendor/autoload.php';
 
-use TYPO3Fluid\Fluid\View\TemplateView;
+function getRandomQuote()
+{
+    $connectionParams = [
+        'dbname' => 'fortunecookies',
+        'user' => 'root',
+        'password' => '',
+        'host' => 'localhost',
+        'driver' => 'pdo_mysql',
+    ];
+    try {
+        $connection = DriverManager::getConnection($connectionParams);
+        $numberPks = $connection
+            ->createQueryBuilder()
+            ->select('COUNT(pk_quote_id) AS NUMBER_PKS')
+            ->from('quote')
+            ->executeQuery()
+            ->fetchAllAssociative();
+        $quote = $connection
+            ->createQueryBuilder()
+            ->select('text')
+            ->from('quote')
+            ->where('pk_quote_id = ' . rand(1, intval($numberPks[0]['NUMBER_PKS'])))
+            ->executeQuery()
+            ->fetchAllAssociative();
+        return $quote[0]['text'];
+    } catch (\Doctrine\DBAL\Exception $e) {
+        return 'DATABASE CONNECTION FAILED';
+    }
+}
 
-$view = new TemplateView();
-$view->assign('foos', array('bar', 'baz'));
-echo $view->render();
+echo getRandomQuote();
